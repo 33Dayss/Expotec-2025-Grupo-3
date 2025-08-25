7c5cc304d03f17fbc6d573007bd481a665bda1f2
33Dayss <147661363+33Dayss@users.noreply.github.com>
2025-08-25 00:07:16 -0300

    siteCodes

---
+++ Scrpthome.js
@@ -0,0 +1,11 @@
+function lginfo(id)
+{
+    var info = document.getElementById(id)
+
+    if(info.style.display === "block") {
+        info.style.display = "none";
+    }
+    else {
+        info.style.display = "block";
+    }
+}
